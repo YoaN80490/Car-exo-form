95efdececafefe7905a155b1c48cde1ff19ccff9
YoaN80490 <yoan.salangros@gmail.com>
2024-06-05 11:35:28 +0200

    back Office marque terminer

--- src/Entity/Brand.php
+++ src/Entity/Brand.php
@@ -6,6 +6,7 @@ use App\Repository\BrandRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: BrandRepository::class)]
 class Brand
@@ -16,6 +17,7 @@ class Brand
     private ?int $id = null;
 
     #[ORM\Column(length: 100)]
+    #[Assert\NotBlank]
     private ?string $name = null;
 
     /**
--- src/Form/CarType.php
+++ src/Form/CarType.php
@@ -6,6 +6,9 @@ use App\Entity\Brand;
 use App\Entity\Car;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,12 +17,19 @@ class CarType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('horsepower')
-            ->add('releasedAt', null, [
-                'widget' => 'single_text',
+            ->add('name', TextType::class, [
+                "label" => "Nom du modèle"
             ])
-            ->add('Color')
+            ->add('horsepower', IntegerType::class, [
+                "label" => "Puissance"
+            ])
+            ->add('releasedAt', DateType::class, [
+                'widget' => 'single_text',
+                "label" => "Date de sortie"
+            ])
+            ->add('Color', TextType::class, [
+                "label" => "Couleur"
+            ])
             ->add('brand', EntityType::class, [
                 'class' => Brand::class,
                 'choice_label' => 'name',
